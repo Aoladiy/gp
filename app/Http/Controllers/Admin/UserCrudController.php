bc7b9eb6fa34e6ba4a7c36bd6a5882db74a313b3
Aldar Ochirov <ochirov-3@mail.ru>
2025-03-16 23:00:08 +0300

    - installed and configured permission manager

---
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Backpack\PermissionManager\app\Http\Controllers\UserCrudController as BaseUserCrudController;
+
+class UserCrudController extends BaseUserCrudController
+{
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use Spatie\Permission\PermissionServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
+    AppServiceProvider::class,
+    PermissionServiceProvider::class
 ];
