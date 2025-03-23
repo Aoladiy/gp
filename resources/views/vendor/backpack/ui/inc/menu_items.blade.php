22a13257111fd74152d2e551d3d6f036604ef8b3
Aldar Ochirov <ochirov-3@mail.ru>
2025-03-23 22:10:23 +0300

    added: - PartTemplates - Parts - PartItems - PartBatches - Equipment - Tags - StorageLocations - StorageZones - StorageMovements - StockMovements - WriteOffReasons - PartItemReturns
    - CrudController for PartTemplates

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -10,3 +10,16 @@
     <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.roles')) }}" icon="la la-group" :link="backpack_url('role')" />
     <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.permission_plural')) }}" icon="la la-key" :link="backpack_url('permission')" />
 </x-backpack::menu-dropdown>
+
+<x-backpack::menu-item title="Part templates" icon="la la-question" :link="backpack_url('part-template')" />
+<x-backpack::menu-item title="Parts" icon="la la-question" :link="backpack_url('part')" />
+<x-backpack::menu-item title="Part items" icon="la la-question" :link="backpack_url('part-item')" />
+<x-backpack::menu-item title="Part batches" icon="la la-question" :link="backpack_url('part-batch')" />
+<x-backpack::menu-item title="Equipment" icon="la la-question" :link="backpack_url('equipment')" />
+<x-backpack::menu-item title="Tags" icon="la la-question" :link="backpack_url('tag')" />
+<x-backpack::menu-item title="Storage locations" icon="la la-question" :link="backpack_url('storage-location')" />
+<x-backpack::menu-item title="Storage zones" icon="la la-question" :link="backpack_url('storage-zone')" />
+<x-backpack::menu-item title="Storage movements" icon="la la-question" :link="backpack_url('storage-movement')" />
+<x-backpack::menu-item title="Stock movements" icon="la la-question" :link="backpack_url('stock-movement')" />
+<x-backpack::menu-item title="Write off reasons" icon="la la-question" :link="backpack_url('write-off-reason')" />
+<x-backpack::menu-item title="Part item returns" icon="la la-question" :link="backpack_url('part-item-return')" />
