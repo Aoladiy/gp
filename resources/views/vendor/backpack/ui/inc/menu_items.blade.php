cb0be0aac9b9ca55cfcb0e4e071f1f88b20f5c69
Aldar Ochirov <ochirov-3@mail.ru>
2025-03-29 17:29:48 +0300

    - reformated menu items

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,25 +1,38 @@
 @php use Illuminate\Support\Str; @endphp
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<x-backpack::menu-item title="{{ Str::ucfirst(trans('backpack::base.dashboard')) }}" icon="la la-home" :link="backpack_url('dashboard')" />
+<x-backpack::menu-item title="{{ Str::ucfirst(trans('backpack::base.dashboard')) }}" icon="la la-home"
+                       :link="backpack_url('dashboard')"/>
 
-<x-backpack::menu-item title="{{ Str::ucfirst(trans('backpack::settings.setting_plural')) }}" icon="la la-cog" :link="backpack_url('setting')" />
+<x-backpack::menu-item title="{{ Str::ucfirst(trans('backpack::settings.setting_plural')) }}" icon="la la-cog"
+                       :link="backpack_url('setting')"/>
 
 <x-backpack::menu-dropdown title="Авторизация" icon="la la-puzzle-piece">
-{{--    <x-backpack::menu-dropdown-header title="Авторизация" />--}}
-    <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.users')) }}" icon="la la-user" :link="backpack_url('user')" />
-    <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.roles')) }}" icon="la la-group" :link="backpack_url('role')" />
-    <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.permission_plural')) }}" icon="la la-key" :link="backpack_url('permission')" />
+    {{--    <x-backpack::menu-dropdown-header title="Авторизация" />--}}
+    <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.users')) }}"
+                                    icon="la la-user" :link="backpack_url('user')"/>
+    <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.roles')) }}"
+                                    icon="la la-group" :link="backpack_url('role')"/>
+    <x-backpack::menu-dropdown-item title="{{ Str::ucfirst(trans('backpack::permissionmanager.permission_plural')) }}"
+                                    icon="la la-key" :link="backpack_url('permission')"/>
 </x-backpack::menu-dropdown>
 
-<x-backpack::menu-item title="Part templates" icon="la la-question" :link="backpack_url('part-template')" />
-<x-backpack::menu-item title="Parts" icon="la la-question" :link="backpack_url('part')" />
-<x-backpack::menu-item title="Part items" icon="la la-question" :link="backpack_url('part-item')" />
-<x-backpack::menu-item title="Part batches" icon="la la-question" :link="backpack_url('part-batch')" />
-<x-backpack::menu-item title="Equipment" icon="la la-question" :link="backpack_url('equipment')" />
-<x-backpack::menu-item title="Tags" icon="la la-question" :link="backpack_url('tag')" />
-<x-backpack::menu-item title="Storage locations" icon="la la-question" :link="backpack_url('storage-location')" />
-<x-backpack::menu-item title="Storage zones" icon="la la-question" :link="backpack_url('storage-zone')" />
-<x-backpack::menu-item title="Storage movements" icon="la la-question" :link="backpack_url('storage-movement')" />
-<x-backpack::menu-item title="Stock movements" icon="la la-question" :link="backpack_url('stock-movement')" />
-<x-backpack::menu-item title="Write off reasons" icon="la la-question" :link="backpack_url('write-off-reason')" />
-<x-backpack::menu-item title="Part item returns" icon="la la-question" :link="backpack_url('part-item-return')" />
+<x-backpack::menu-dropdown title="Запчасти" icon="la la-puzzle-piece">
+    <x-backpack::menu-dropdown-item title="Шаблоны запчастей" icon="la la-question"
+                                    :link="backpack_url('part-template')"/>
+    <x-backpack::menu-dropdown-item title="Запчасти" icon="la la-question" :link="backpack_url('part')"/>
+    <x-backpack::menu-dropdown-item title="Экземпляры запчастей" icon="la la-question"
+                                    :link="backpack_url('part-item')"/>
+    <x-backpack::menu-dropdown-item title="Партии запчастей" icon="la la-question" :link="backpack_url('part-batch')"/>
+</x-backpack::menu-dropdown>
+<x-backpack::menu-item title="Техника" icon="la la-question" :link="backpack_url('equipment')"/>
+<x-backpack::menu-item title="Теги" icon="la la-question" :link="backpack_url('tag')"/>
+<x-backpack::menu-dropdown title="Склад" icon="la la-puzzle-piece">
+    <x-backpack::menu-dropdown-item title="Складские локации" icon="la la-question" :link="backpack_url('storage-location')"/>
+    <x-backpack::menu-dropdown-item title="Складские зоны" icon="la la-question" :link="backpack_url('storage-zone')"/>
+    <x-backpack::menu-dropdown-item title="Складские перемещения" icon="la la-question"
+                           :link="backpack_url('storage-movement')"/>
+    <x-backpack::menu-dropdown-item title="Складские движения" icon="la la-question" :link="backpack_url('stock-movement')"/>
+</x-backpack::menu-dropdown>
+<x-backpack::menu-item title="Причины списания" icon="la la-question" :link="backpack_url('write-off-reason')"/>
+<x-backpack::menu-item title="Возврат экземпляров запчастей" icon="la la-question"
+                       :link="backpack_url('part-item-return')"/>
