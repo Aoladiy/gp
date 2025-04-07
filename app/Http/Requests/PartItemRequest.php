64581298175416f667dc084185351e5d12c19e25
Aldar Ochirov <ochirov-3@mail.ru>
2025-04-07 09:43:44 +0300

    minor fixes and added image field to PartItem

--- app/Http/Requests/PartItemRequest.php
+++ app/Http/Requests/PartItemRequest.php
@@ -25,7 +25,8 @@ class PartItemRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+             'part_id' => 'required',
+             'status_id' => 'required',
         ];
     }
 
